ae8435ce471dff50e094e03d3df0ea3abf8f1567
Yonatan Karp-Rudin <yonvata@gmail.com>
2022-12-09 15:29:44 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("jacoco")
+    id("pmd")
+    id("spring-boot-app-template.java-conventions")
+    id("spring-boot-app-template.code-metrics")
+    id("spring-boot-app-template.publishing-conventions")
+    id("com.diffplug.spotless") version "6.12.0" apply true
+    id("org.springframework.boot") version "3.0.0" apply false
+    id("io.spring.dependency-management") version "1.1.0" apply false
+    kotlin("jvm") version "1.7.22" apply false
+    kotlin("plugin.spring") version "1.7.22" apply false
+}
+
+subprojects {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        maven { url = uri("https://packages.confluent.io/maven/") }
+        maven {
+            url = uri("https://maven.pkg.github.com/yonatankarp/spring-boot-app-template")
+            credentials {
+                username = findProperty("gpr.user")?.toString() ?: System.getenv("GITHUB_ACTOR")
+                password = findProperty("gpr.key")?.toString() ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
---
+++ spring-boot-app-template/build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    id("jacoco")
+    id("spring-boot-app-template.code-metrics")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    kotlin("jvm")
+    kotlin("plugin.spring")
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+val coroutinesVersion = "1.6.4"
+val mockkVersion = "1.13.2"
+val mockkSpringVersion = "3.1.1"
+
+dependencies {
+    // Spring Boot
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    // Kotlin
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Tests
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks {
+    getByName<Jar>("jar") {
+        enabled = false
+    }
+
+    build {
+        finalizedBy(spotlessApply)
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+        finalizedBy(spotlessApply)
+        finalizedBy(jacocoTestReport)
+        finalizedBy(pmdTest)
+    }
+
+    jacoco {
+        toolVersion = "0.8.7"
+    }
+
+    jacocoTestReport {
+        reports {
+            xml.required.set(true)
+            html.required.set(true)
+        }
+    }
+}
